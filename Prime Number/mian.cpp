832f11f25cb8ebfa5ccb9914d7c61635359c4571
Sasha-705 <aleks7052008@gmail.com>
2025-12-21 13:28:45 +0100

    Prime Number V2

--- Prime Number/mian.cpp
+++ Prime Number/mian.cpp
@@ -3,40 +3,47 @@
 
 void clearInput()
 {
-	std::cin.clear();
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
 int main()
 {
-	unsigned int prime;
-	int count = 0;
+    unsigned int n;
 
-	std::cout << "Write a natural number: ";
-	std::cin >> prime;
-	while (std::cin.fail())
-	{
-		clearInput();
-		std::cout << "Write a valid input: ";
-		std::cin >> prime;
-	}
-	for (int i = 1; i <= prime; ++i)
-	{
-		float check = prime % i;
-		if (check == 0)
-		{
-			count++;
-		}
-	}
+    std::cout << "Write a natural number: ";
+    std::cin >> n;
 
-	if (count == 2)
-	{
-		std::cout << "The number " << prime << " is prime" << std::endl;
-	}
-	else
-	{
-		std::cout << "The number " << prime << " is not prime" << std::endl;
-	}
-	
-	return 0;
+    while (std::cin.fail())
+    {
+        clearInput();
+        std::cout << "Write a valid input: ";
+        std::cin >> n;
+    }
+
+    // Handle 0 and 1 explicitly
+    if (n <= 1)
+    {
+        std::cout << "The number " << n << " is not prime\n";
+        return 0;
+    }
+
+    bool isPrime = true;
+
+    // Check divisors only up to sqrt(n)
+    for (unsigned int i = 2; i * i <= n; ++i)
+    {
+        if (n % i == 0)
+        {
+            isPrime = false;
+            break; // no need to continue
+        }
+    }
+
+    if (isPrime)
+        std::cout << "The number " << n << " is prime\n";
+    else
+        std::cout << "The number " << n << " is not prime\n";
+
+    return 0;
 }
